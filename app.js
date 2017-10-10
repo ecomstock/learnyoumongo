714b334f97674822ce47abff5d120eaad9ad7cf4
Eric <ecomstock@gmail.com>
2017-10-09 20:04:30 -0700

    Completed learnyoumongo 4.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//learnyoumongo 3
+//learnyoumongo 4
 let mongo = require('mongodb').MongoClient
 let url = 'mongodb://localhost:27017/learnyoumongo'
 let baselineAge = process.argv[2]
@@ -10,9 +10,32 @@ mongo.connect(url, (err, db) => {
 		age: {
 			$gt: +baselineAge
 		}
+	}, {
+		name: 1,
+		age: 1,
+		_id: 0
 	}).toArray((err, docs) => {
 		if (err) throw err
 		console.log(docs)
 		db.close()
 	})
 })
+
+// //learnyoumongo 3
+// let mongo = require('mongodb').MongoClient
+// let url = 'mongodb://localhost:27017/learnyoumongo'
+// let baselineAge = process.argv[2]
+
+// mongo.connect(url, (err, db) => {
+// 	if (err) throw err
+// 	let parrots = db.collection('parrots')
+// 	parrots.find({
+// 		age: {
+// 			$gt: +baselineAge
+// 		}
+// 	}).toArray((err, docs) => {
+// 		if (err) throw err
+// 		console.log(docs)
+// 		db.close()
+// 	})
+// })
