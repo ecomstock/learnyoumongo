c20c0407c2e570555ae498bcae3ec1ccf7a897b2
Eric <ecomstock@gmail.com>
2017-10-10 10:29:21 -0700

    Completed learnyoumongo 5.

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
-//learnyoumongo 4
+//learnyoumongo 5
 let mongo = require('mongodb').MongoClient
 let url = 'mongodb://localhost:27017/learnyoumongo'
-let baselineAge = process.argv[2]
+let firstName = process.argv[2]
+let lastName = process.argv[3]
+let doc = {
+	firstName: firstName,
+	lastName: lastName
+}
 
 mongo.connect(url, (err, db) => {
 	if (err) throw err
-	let parrots = db.collection('parrots')
-	parrots.find({
-		age: {
-			$gt: +baselineAge
-		}
-	}, {
-		name: 1,
-		age: 1,
-		_id: 0
-	}).toArray((err, docs) => {
+	let docs = db.collection('docs')
+	docs.insert(doc, (err, data) => {
 		if (err) throw err
-		console.log(docs)
+		console.log(JSON.stringify(doc))
 		db.close()
 	})
 })
+
+
+// //learnyoumongo 4
+// let mongo = require('mongodb').MongoClient
+// let url = 'mongodb://localhost:27017/learnyoumongo'
+// let baselineAge = process.argv[2]
+
+// mongo.connect(url, (err, db) => {
+// 	if (err) throw err
+// 	let parrots = db.collection('parrots')
+// 	parrots.find({
+// 		age: {
+// 			$gt: +baselineAge
+// 		}
+// 	}, {
+// 		name: 1,
+// 		age: 1,
+// 		_id: 0
+// 	}).toArray((err, docs) => {
+// 		if (err) throw err
+// 		console.log(docs)
+// 		db.close() 
+// 	})
+// })
 
 // //learnyoumongo 3
 // let mongo = require('mongodb').MongoClient
